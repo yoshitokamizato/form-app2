d5e8af38f125a01eec698e75bfe4ba8bb050caff
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-06-08 15:11:30 +0900

    add import and export method to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,32 @@
 class User < ApplicationRecord
+
+
   serialize :skils
 
   validates :name, presence: true
   validates :age, presence: true
-  validates :mail, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, presence: true
+  validates :mail, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, presence: true
   validates :address, presence: true
   validates :gender, presence: true
   validates :program, presence: true
   validates :skils, presence: true
 
-  has_many :user_programmings, foreign_key: 'user_id'  # 中間テーブルを介したデータの取り出し
-  has_many :programmings, through: :user_programmings  # 中間テーブルを介したデータの取り出し
+  has_many :user_programmings, foreign_key: 'user_id' # 中間テーブルを介したデータの取り出し
+  has_many :programmings, through: :user_programmings # 中間テーブルを介したデータの取り出し
 
-  def searchAll
-    return User.all
+  def self.import(path)
+    CSV.foreach(path, headers: true) do |row|
+      p row
+    end
   end
 
-  def searchId(id1,id2)
-    return User.find_by_sql(['select * from users where id >= :id1 and id <= :id2', {id1: id1, id2: id2}])
-  end
-
-  def searchData(user)
-    return User.find_by_sql(['SELECT * FROM users WHERE CONCAT(id, name, address, gender, program, skils) LIKE :user', {user: "%#{user}%"}])
+  def self.export
+    CSV.open("write-sample2.csv", "w") do |test|
+      test << ["language", "product"]
+      test << ["ruby", "web_application"]
+      test << ["python", "AI"]
+      test << ["java", "business_application"]
+    end
   end
 
   mount_uploader :image, ImageUploader
@@ -40,14 +45,16 @@ class User < ApplicationRecord
   def geometry
     @geometry ||= _geometry
   end
+
   private
+
   # ------------------------------------------------------------------
   # Private Instance Methods
   # ------------------------------------------------------------------
   def _geometry
     if image.file and File.exists?(image.file.file)
       img = ::Magick::Image::read(image.file.file).first
-      { width: img.columns, height: img.rows }
+      {width: img.columns, height: img.rows}
     end
   end
 end
