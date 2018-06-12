b47b329ba32f88887ca787bd06ef3cd110eefa19
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-06-12 21:26:51 +0900

    add validation of user_programmings

--- app/models/user_programming.rb
+++ app/models/user_programming.rb
@@ -3,5 +3,6 @@ class UserProgramming < ApplicationRecord
   belongs_to :programming
 
   validates :user_id, :uniqueness => {:scope => :name}
-  
+  validates :name, presence: true
+  validates :level, presence: true
 end
